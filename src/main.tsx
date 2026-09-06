24b36a3ae335f957d9575e4d99d52f005f58a9ec
SST-CRUD <sstdevelopaminno@gmail.com>
2026-09-07 00:49:53 +0700

    style: load POS shortcut handler

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./pos-keyboard-shortcuts";
 import "./styles.css";
 import "./retail-ui.css";
 import "./receipt-print.css";
