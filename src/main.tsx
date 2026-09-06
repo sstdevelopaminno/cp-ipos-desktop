7e4859cadb25e353549edbd0b4cf3a6db7103ef8
SST-CRUD <sstdevelopaminno@gmail.com>
2026-09-07 00:18:27 +0700

    style: load sales empty state branding

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./retail-ui.css";
 import "./receipt-print.css";
+import "./grocery-empty-branding.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
