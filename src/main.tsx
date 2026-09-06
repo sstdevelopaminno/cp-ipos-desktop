ca832eb67dd3aaf072d01eed492c459df558e775
SST-CRUD <sstdevelopaminno@gmail.com>
2026-09-07 01:14:31 +0700

    style: load inventory barcode enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./pos-keyboard-shortcuts";
+import "./inventory-barcode-enhancements";
 import "./styles.css";
 import "./retail-ui.css";
 import "./receipt-print.css";
 import "./grocery-empty-branding.css";
 import "./sales-scan-qty-ui.css";
+import "./inventory-barcode-enhancements.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
